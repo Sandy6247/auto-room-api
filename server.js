e56fde94a08731976aad0f83cab63f9b99dfcd48
Sandy Labalette <sandylabalette@gmail.com>
2022-08-16 12:30:06 +0200

    init server.js and add env

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("salut toto");
+import express from 'express';
+const app = express();
+
+const port = 3000;
+const env = 'development';
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`⚡ listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode ⚡`);
+});
